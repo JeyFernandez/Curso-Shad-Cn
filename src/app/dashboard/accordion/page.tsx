181fb4143730237c0bfaeff5ec6a463ac41ab182
Jasson Fernandez <jeicobfer@gmail.com>
2024-06-27 06:15:15 -0600

    Creando las vistas del dashboard

---
+++ src/app/dashboard/accordion/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Accordion Page</h1>
+      <p>
+        This is an accordion page. It is a placeholder for the real accordion
+        page.
+      </p>
+    </div>
+  );
+}
---
+++ src/app/dashboard/alert/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Alert Page</h1>
+      <p>This is an alert page. It is a placeholder for the real alert page.</p>
+    </div>
+  );
+}
